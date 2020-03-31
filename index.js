07caac1e6d5f1a0ac23cbbb3cd2a48e9f5d3780f
4lon3ly0 <guillem0907rgmail.com>
2020-03-31 13:53:51 +0200

    Added the new commands

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const clc = require('cli-color');
 
 const bot = new Discord.Client();
 
-const token = 'NjYyMzM0MzIwODc0MjI1Njc0.Xno4JA.L2Ukf_v7RGm6drv3qPIMZjEThi0';
+const token = 'TOKEN';
 
-const PREFIX = 'v!'
+const PREFIX = 'v!';
 
 const fs = require('fs');
 
@@ -26,25 +26,31 @@ bot.on('ready', () => {
   bot.user.setActivity('Matar | v!help = info')
 });
 
-bot.on('message', message => {
+bot.on('message', async message => {
   if (message.content.startsWith(PREFIX)) {
   let args = message.content.substring(PREFIX.length).split(" ");
-
+  if (message.guild.name === 'Golden Exilie') {message.channel.send('No voy a volver a responder comandos dentro de este server');
+  return;}
   switch(args[0]){
+     case 'teshin':
+      message.delete().catch();
+      message.channel.send('', {files: ['./img/teshin.png']});
+    break;
+
      case 'micreador':
-      bot.commands.get('micreador').execute(message, args);
+       bot.commands.get('micreador').execute(message, args);
     break;
 
      case 'help': 
-      bot.commands.get('help').execute(message, args, bot);
+       bot.commands.get('help').execute(message, args, bot);
     break;
     
      case 'condenar':
-      bot.commands.get('condenar').execute(message, args);
+       bot.commands.get('condenar').execute(message, args);
     break;
 
      case 'micodigo':
-      message.channel.send("Soy tanto de codigo abierto como de software libre. Cualquier duda: `guillem0907@gmail.com` Mi codigo fuente: https://github.com/alonely0/verdugo");
+       message.channel.send("Soy tanto de codigo abierto como de software libre. Cualquier duda: `guillem0907@gmail.com` Mi codigo fuente: https://github.com/alonely0/verdugo");
     break;
 
      case 'serverinfo':
@@ -52,43 +58,48 @@ bot.on('message', message => {
     break;
 
     case 'ban':
-      if (message.member.roles.find("name", "Monarca")){
-       var mentioned = message.mentions.users.first();
-       let reason = args.slice(1).join(' ');
-       message.guild.ban(mentioned, {reason: reason});
-     }else{
-      message.channel.send('Tienes que tener el rol `Monarca` para poder usar este comando');
-     };
+      bot.commands.get('ban').execute(message, args)
     break;
     
-     case 'botinfo':
+    case 'botinfo':
        bot.commands.get('botinfo').execute(message, args, bot);
     break;
 
-     case 'warn':
+    case 'warn':
        bot.commands.get('warn').execute(message, args);
     break;
 
-     case 'web':
+    case 'web':
        message.channel.send('La web: https://verdugo-discord-bot.webnode.es');
     break;
      
-     case 'inv':
+    case 'inv':
        message.channel.send('Enlace de invitacion del bot: https://discordapp.com/api/oauth2/authorize?client_id=662334320874225674&permissions=8&scope=bot');
     break;
 
-     case 'server':
+    case 'server':
        message.channel.send('Mi servidor: https://discord.gg/Fs4EKJE');
     break;
 
-     case 'say':
-      var sayMessage = args.slice(1).join(' ')
-      message.delete().catch();
-      message.channel.send(sayMessage);
+    case 'say':
+      bot.commands.get('say').execute(message, args);
     break;
-  }
+
+    case 'parca':
+      bot.commands.get('parca').execute(message, args);
+    break;
+  
+    case 'clear':
+      bot.commands.get('clear').execute(message, args);
+    break;
+
+    case 'unban':
+      bot.commands.get('unban').execute(message, args);
+    break;
+    }
       }else{
       return;
+      
       };
     });
 bot.login(token);
