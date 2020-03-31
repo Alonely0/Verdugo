4fa21747108822b48acada8781fc0fb20c02a5a2
4lon3ly0 <guillem0907rgmail.com>
2020-03-31 14:01:17 +0200

    New commands

--- commands/ban.js
+++ commands/ban.js
@@ -8,6 +8,6 @@ if (message.member.roles.find("name", "Monarca")){
     message.guild.ban(mentioned, {reason: reason});
     message.channel.send('Es preferible el `v!condenar`, este avisa al condenado con los motivos y guarda el ban con el autor, baneado y motivo en los registros del bot.')
   }else{
-   message.channel.send('Tienes que tener el rol `Monarca` para poder usar este comando');
+   message.channel.send('Tienes que tener el rol `Monarca` para poder usar este comando.');
   };
 }};
