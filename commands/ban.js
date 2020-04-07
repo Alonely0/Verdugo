bef45ef25ca7cfdad361addd317582a73bef827f
4lon3ly0 <guillem0907rgmail.com>
2020-04-07 13:10:27 +0200

    Stabilized

--- commands/ban.js
+++ commands/ban.js
@@ -7,6 +7,7 @@ if (message.member.roles.find("name", "Monarca")){
     var mentioned = message.mentions.users.first();
     let reason = args.slice(1).join(' ');
     message.guild.ban(mentioned, {reason: reason});
+    message.channel.send('Usuario baneado.');
     message.channel.send('Es preferible el `v!condenar`, este avisa al condenado con los motivos y guarda el ban con el autor, baneado y motivo en los registros del bot.')
   }else{
    message.channel.send('Tienes que tener el rol `Monarca` para poder usar este comando.');
