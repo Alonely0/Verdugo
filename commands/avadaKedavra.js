490aaed482c27c0db5048db979b6fcbd14d53feb
4lon3ly0 <guillem0907rgmail.com>
2020-04-08 23:55:02 +0200

    Added special embeds with '@here' and '@everyone'

--- commands/avadaKedavra.js
+++ commands/avadaKedavra.js
@@ -6,6 +6,22 @@ module.exports = {
         var user = message.mentions.users.first()
         message.delete()
         const Discord = require('discord.js')
+        if (user === 'undefined' || '' || message.content.slice(15) === '') {
+         return message.reply('proporciona un usuario valido.');}
+         if (message.content.slice(4) === '@everyone') {
+             let Embed3 = new Discord.RichEmbed()
+             .setColor('#ff0000  ')
+             .setTitle(`Avada kedavra`)
+             .setDescription(`${message.author} ha usado la maldicion asesina contra todo el mundo`)
+             .setImage("https://media1.tenor.com/images/b09013f18bbb60a866083d764a5145e9/tenor.gif?itemid=12401009"); 
+         return message.channel.send(Embed3);}
+         if (message.content.slice(15) === '@here') {
+            let Embed3 = new Discord.RichEmbed()
+            .setColor('#ff0000  ')
+            .setTitle(`Avada kedavra`)
+            .setDescription(`${message.author} ha usado la maldicion asesina contra todos los testigos.`)
+            .setImage("https://media1.tenor.com/images/b09013f18bbb60a866083d764a5145e9/tenor.gif?itemid=12401009"); 
+        return message.channel.send(Embed3);}
         let Embed = new Discord.RichEmbed()
             .setColor('#ff0000  ')
             .setTitle(`Avada kedavra`)
--- commands/f.js
+++ commands/f.js
@@ -6,8 +6,22 @@ module.exports = {
         var user = message.mentions.users.first()
         message.delete()
         const Discord = require('discord.js')
+        if (user === 'undefined' || '' || message.content.slice(4) === '') {
+           let Embed2 = new Discord.RichEmbed()
+            .setColor('#ff0000')
+            .setTitle(`*F*`)
+            .setDescription(`**F**`)
+            .setImage("https://cdn.dribbble.com/users/1523131/screenshots/4590296/f_typo_2.gif");
+        return message.channel.send(Embed2);}
+        if (message.content.slice(4) === '@everyone' || '@here') {
+            let Embed3 = new Discord.RichEmbed()
+            .setColor('#ff0000')
+            .setTitle(`*F*`)
+            .setDescription(`**F** por todos`)
+            .setImage("https://cdn.dribbble.com/users/1523131/screenshots/4590296/f_typo_2.gif"); 
+        return message.channel.send(Embed3);}
         let Embed = new Discord.RichEmbed()
-            .setColor('#ff0000  ')
+            .setColor('#ff0000')
             .setTitle(`*F*`)
             .setDescription(`**F** por ${user}.`)
             .setImage("https://cdn.dribbble.com/users/1523131/screenshots/4590296/f_typo_2.gif");
