3aea025de7c518830e36313a17f4fa66dfee535b
4lon3ly0 <guillem0907rgmail.com>
2020-04-08 23:55:53 +0200

    Changed password and added it to config.json

--- commands/bugreport.js
+++ commands/bugreport.js
@@ -1,14 +1,15 @@
 module.exports = {
     name: 'bugreport',
     description: "Reporta un bug.",
-    execute(message, args){
+    execute(message, args, config, bot){
 var Discord = require('discord.js');
 var nodemailer = require('nodemailer');
 var moment = require('moment');
+const password = config.verdugobugreport
 var mailText = message.content.slice(12);
 var bugEmbed = new Discord.RichEmbed()
 .setTitle('**__Bug Report__**')
-.setDescription(`${message.author} ha hecho un bug report oficial. Si el bug report no contenia un bug, esta a tiempo de enviar otro disculpandose, en caso contrario, no podra volver a usar el bot.`)
+.setDescription(`${message.author} ha hecho un bug report oficial. Si el bug report no contenia un bug, esta a tiempo de enviar otro disculpandose, en caso contrario, no podra volver a usar el bot. Asegurese de estar en el servidor de soporte (\`v!server\`) por si se necesita mas informacion.`)
 .addField('Autor:', `${message.author.tag}`)
 .addField('Bug:', `${mailText}`)
 .setImage('https://cdn.dribbble.com/users/720472/screenshots/2055709/send-email.gif')
@@ -18,7 +19,7 @@ var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: 'verdugobugreport@gmail.com',
-    pass: '..100Verdugo100'
+    pass: password
   }
 });
 
@@ -31,7 +32,7 @@ var mailOptions = {
 
 transporter.sendMail(mailOptions, function(error, info){
   if (error) {
-      message.reply('ha habido un error con el envio, contacta manualmente con `v!server`.');
+      message.reply('ha habido un error con el envio, porfavor, contacta manualmente con `v!server`.');
     console.log(error);
   } else {
     message.channel.send(bugEmbed);
