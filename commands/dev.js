5f54cdb115ab8cf80005cee5d394e26dcef839a4
4lon3ly0 <guillem0907rgmail.com>
2020-04-11 05:59:09 +0200

    New gifs

--- commands/dev.js
+++ commands/dev.js
@@ -2,20 +2,23 @@ module.exports = {
     name: "dev",
     description: "Comandos de desarrollador.",
     execute(message, args, bot) {
-
-    if (message.author.id != 615199347398672392) return message.channel.send('Este es un comando de desarrollador, si vuelves a utilizarlo sin serlo no podras volver a usarme.');  
-    message.delete()
+        
+    if (message.author.id != 615199347398672392) {return message.channel.send('', {files: ['./img/fingerprint-scan-400h-deneid.gif']});
+    }else{message.channel.send('', {files: ['./img/fingerprint-scan-400h-granted.gif']});}
     const act = message.content.slice(16);
         switch(args[1]) {
             case 'shutdown':
-                setTimeout(function(){ process.exit(); }, 2000);
+                setTimeout(function(){ message.channel.send('', {files: ['./img/153083920502.gif']}); }, 2000);
+                setTimeout(function(){ process.exit(); }, 3000);
             break;
 
             case 'actividad':
+                setTimeout(function(){ message.channel.send('', {files: ['./img/descargar (1).gif']}); }, 2000);
                 bot.user.setActivity(act);
             break;
 
             case 'estado':
+              setTimeout(function(){ message.channel.send('', {files: ['./img/descargar (1).gif']}); }, 2000);
                 switch(args[2]) {
                     case 'online':
                         bot.user.setStatus('online');
