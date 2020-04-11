1ca5044a8806c8ca34fec3e2617ffe5488d6fd12
4lon3ly0 <guillem0907rgmail.com>
2020-04-11 06:01:51 +0200

    Now also send the message to a specific channel in the support server.

--- commands/bugreport.js
+++ commands/bugreport.js
@@ -5,16 +5,25 @@ module.exports = {
 var Discord = require('discord.js');
 var nodemailer = require('nodemailer');
 var moment = require('moment');
+const bugChannel = bot.channels.find('id', '698213868056608918')
 const password = config.verdugobugreport
 var mailText = message.content.slice(12);
 var bugEmbed = new Discord.RichEmbed()
-.setTitle('**__Bug Report__**')
-.setDescription(`${message.author} ha hecho un bug report oficial. Si el bug report no contenia un bug, esta a tiempo de enviar otro disculpandose, en caso contrario, no podra volver a usar el bot. Asegurese de estar en el servidor de soporte (\`v!server\`) por si se necesita mas informacion.`)
-.addField('Autor:', `${message.author.tag}`)
-.addField('Bug:', `${mailText}`)
-.setImage('https://cdn.dribbble.com/users/720472/screenshots/2055709/send-email.gif')
-.setFooter(`En: ${moment().format("dddd, MMMM Do YYYY, h:mm A", Date.now())}`)
-.setColor('#ff0000');
+ .setTitle('**__Bug Report__**')
+ .setDescription(`${message.author} ha hecho un bug report oficial. Si el bug report no contenia un bug, esta a tiempo de enviar otro disculpandose, en caso contrario, no podra volver a usar el bot. Asegurese de estar en el servidor de soporte (\`v!server\`) por si se necesita mas informacion.`)
+ .addField('Autor:', `${message.author.tag}, (${message.author.id})`)
+ .addField('Bug:', `${mailText}`)
+ .setImage('https://cdn.dribbble.com/users/720472/screenshots/2055709/send-email.gif')
+ .setFooter(`En: ${moment().format("dddd, MMMM Do YYYY, h:mm A", Date.now())}`)
+ .setColor('#ff0000');
+var bugEmbed_ = new Discord.RichEmbed()
+ .setTitle('**__Bug Report__**')
+ .setDescription(`${message.author} ha hecho un bug report oficial. Si el bug report no contenia un bug, esta a tiempo de enviar otro disculpandose, en caso contrario, no podra volver a usar el bot.`)
+ .addField('Autor:', `${message.author.tag}, (${message.author.id})`)
+ .addField('Bug:', `${mailText}`)
+ .setImage('https://media3.giphy.com/media/l3q2KRkOVYvi8WfU4/giphy.gif?cid=ecf05e4797f9bf1f87295f1cbb28ab5cb93e350e5cb2ab81&rid=giphy.gif')
+ .setFooter(`En: ${moment().format("dddd, MMMM Do YYYY, h:mm A", Date.now())}`)
+ .setColor('#ff0000');
 var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
@@ -36,6 +45,7 @@ transporter.sendMail(mailOptions, function(error, info){
     console.log(error);
   } else {
     message.channel.send(bugEmbed);
+    bugChannel.send(bugEmbed_);
     console.log('Email sent: ' + info.response);
   }});
     }};
