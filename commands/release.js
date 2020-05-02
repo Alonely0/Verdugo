037278ea3ca2852bf6de75e5774a491520d943d8
4lon3ly0 <guillem0907rgmail.com>
2020-05-02 14:22:52 +0200

    Updated with de new version (6.3)

--- commands/release.js
+++ commands/release.js
@@ -6,9 +6,10 @@ module.exports = {
       let bicon = bot.user.displayAvatarURL;
       let release = new Discord.RichEmbed()
        .setThumbnail(bicon)
-       .setTitle('Actualizacion a la version **6.1**')
-       .setDescription('Informacion de la actualizacion 6.1')
-       .addField('Comandos Añadidos:', '`v!deep`')
+       .setTitle('Actualizacion a la version **6.3**')
+       .setDescription('Informacion de la actualizacion 6.3')
+       .addField('Comandos Añadidos:', '`v!NOT-`')
+       .addField('Pequeños detalles:', 'Se han arreglado algunos bugs con los comandos de banear y condenar. Se ha disminuido la latencia.')
        .setImage('https://cdn.discordapp.com/attachments/665290737256103961/698510102260351016/Verdugo_Tamano_original.png')
        .setColor('#ff0000');
     message.channel.send(release);
