5d1d19e28c238bf1abe49c342ea2e2f823ec5112
4lon3ly0 <guillem0907rgmail.com>
2020-05-03 18:49:04 +0200

    Updated with de new version

--- commands/release.js
+++ commands/release.js
@@ -6,10 +6,10 @@ module.exports = {
       let bicon = bot.user.displayAvatarURL;
       let release = new Discord.RichEmbed()
        .setThumbnail(bicon)
-       .setTitle('Actualizacion a la version **6.3**')
-       .setDescription('Informacion de la actualizacion 6.3')
-       .addField('Comandos Añadidos:', '`v!NOT-`')
-       .addField('Pequeños detalles:', 'Se han arreglado algunos bugs con los comandos de banear y condenar. Se ha disminuido la latencia.')
+       .setTitle('Actualizacion a la version **6.6**')
+       .setDescription('Informacion de la actualizacion 6.6')
+       .addField('Comandos Añadidos:', '`v!ouija` | `v!8ball` | `v!curse`')
+       .addField('Cambio de Hosting:', 'Debido a que no he podido verificar la cuenta del hosting, el bot no habria estado 24/7 en Heroku. Es por eso que he cambiado el hosting a Glitch, donde espero no tener mas problemas.')
        .setImage('https://cdn.discordapp.com/attachments/665290737256103961/698510102260351016/Verdugo_Tamano_original.png')
        .setColor('#ff0000');
     message.channel.send(release);
