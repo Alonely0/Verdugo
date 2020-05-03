b1da121db5f3d8739ab759bcc49d425b3168e1cc
4lon3ly0 <guillem0907rgmail.com>
2020-05-03 18:50:16 +0200

    Bug fixes

--- commands/poseer.js
+++ commands/poseer.js
@@ -2,14 +2,16 @@ module.exports = {
     name: 'poseer',
     description: 'Satan.',
 execute(message, args) {
+  try {
     const Discord = require('discord.js');
     message.delete().catch();
     var mentioned = message.mentions.users.first();
     if (!mentioned) return message.channel.send('Menciona a un usuario');
-    var laParca = new Discord.RichEmbed()
+    var poseido = new Discord.RichEmbed()
     .setColor('#ff0000')
     .setTitle('**Poseido**')
     .setDescription(`${mentioned} ha sido poseido por un mal mayor.`)
     .setImage('https://media1.tenor.com/images/347d5962c76a4fcf771fe466ab93f5c9/tenor.gif?itemid=5394962');
-    message.channel.send(laParca);
+    message.channel.send(poseido);
+  }catch(err) { message.channel.send('error'); }
 }};
